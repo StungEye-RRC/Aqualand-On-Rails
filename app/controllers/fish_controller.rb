eeda5679130fb52e11979ab17ae093f085ac8794
Kyle Geske <stungeye@gmail.com>
2018-10-31 10:06:51 -0500

    Implemented the index and show fish controller actions.

--- app/controllers/fish_controller.rb
+++ app/controllers/fish_controller.rb
@@ -1,7 +1,18 @@
 class FishController < ApplicationController
+  # Collection action for all fish.
+  # GET /fish
+  # GET /
   def index
+    @fish_collection = Fish.order(:name)
   end
+  # automagically share @ vars with
+  # /app/views/fish/index.html.erb
 
+  # Member action for a specific fish by id.
+  # GET /fish/:id
   def show
+    @fish = Fish.find(params[:id])
   end
+  # automagically share @ vars with
+  # /app/views/fish/show.html.erb
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'fish/index'
-  get 'fish/show'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :fish, only: [:index, :show]
+  root to: 'fish#index'
 end
